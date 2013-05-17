206e32458763aaee422732c3d91a8b4b3c7c430c
Josiah Ebhomenye <jay@USERs-MacBook-Air.local>
2013-05-17 22:20:29 +0100

    added dependency inject container

--- console/src/main/java/com/jebhomenye/log4j/service/LoggerService.java
+++ console/src/main/java/com/jebhomenye/log4j/service/LoggerService.java
@@ -1,6 +1,12 @@
 package com.jebhomenye.log4j.service;
 
+import com.google.inject.Inject;
+import com.jebhomenye.log4j.store.DataStore;
+
 public class LoggerService {
+	
+	@Inject
+	private DataStore dataStore;
 	
 	public void log(String appName, String event, long time){
 		
